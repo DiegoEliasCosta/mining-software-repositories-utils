a9e4e723e2ba4f23d547c6774f0a8a8f981b8ea5
Diego Costa <diegoelias1@gmail.com>
2019-10-10 10:15:29 -0400

    Updating to account for the max number of stars

--- github/find-1k-most-starred.py
+++ github/find-1k-most-starred.py
@@ -8,10 +8,11 @@ from github import Github
 @click.command()
 @click.argument("username", required=1)
 @click.argument("password", required=1)
+@click.option("--max_stars", default=None, help="The maximum number of stars - as we order the query, this can be used to find projects below the 1k most starred")
 @click.option("--n_pages", default=35, help="The number of pages the request will go over (typically 33) as Github has a limit of 1000 results")
 @click.option("--language", default="java", help="The target programming language")
 @click.option("--output", default=".", help="The directory where the output will be saved")
-def find_1k_most_starred_repos(username, password, n_pages, language, output):
+def find_most_starred_repos(username, password, max_stars, n_pages, language, output):
     """ Find the most starred repositories using an authenticated GiHub USERNAME and PASSWORD """
 
 
@@ -19,14 +20,21 @@ def find_1k_most_starred_repos(username, password, n_pages, language, output):
     gh_session = requests.Session()
     gh_session.auth = (username, password)
 
-    go_url = "https://api.github.com/search/repositories?q=language:{language}&stars:1..*&sort=stars&order=desc&page={page}"
+    if not max_stars:
+        max_stars = '*'
+
+
+    go_url = "https://api.github.com/search/repositories?q=language:{language}+stars:1..{max_stars}&sort=stars&order=desc&page={page}"
+
+    
 
     df = pd.DataFrame()
 
     for page in range(1, n_pages):
 
-            req_url = go_url.format(language=language, page=page)
+            req_url = go_url.format(language=language, page=page, max_stars=max_stars)
 
+            print("Querying {query}".format(query=req_url))
             response = gh_session.get(req_url)
 
             if response:
@@ -43,11 +51,11 @@ def find_1k_most_starred_repos(username, password, n_pages, language, output):
 
     # We pickle first in case we find some issues with converting to JSON  
     # such as non-unique indexes
-    df.to_pickle(os.path.join(output, '1kmoststarred-{language}-projects.pickle'.format(language=language)))
+    df.to_pickle(os.path.join(output, 'moststarred-{language}-projects-{max_stars}.pickle'.format(language=language, max_stars=max_stars)))
 
     # Making sure we 
     df.reset_index(inplace=True)
-    df.to_json(os.path.join(output, '1kmoststarred-{language}-projects.json'.format(language=language)))
+    df.to_json(os.path.join(output, 'moststarred-{language}-projects-{max_stars}.json'.format(language=language, max_stars=max_stars)))
 
 
 def sleep_random():
@@ -58,5 +66,5 @@ def sleep_random():
 
 if __name__ == "__main__":
     # pylint: disable=no-value-for-parameter
-    find_1k_most_starred_repos()
+    find_most_starred_repos()
     pass
